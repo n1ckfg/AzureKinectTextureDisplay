8da9a6aace72473866a54ddaa4ee5d7147453185
Nick Fox-Gieg <nick@fox-gieg.com>
2022-07-02 16:04:55 -0400

    gl settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,29 @@
+#include "ofMain.h"
 #include "ofApp.h"
 
+//========================================================================
 int main() {
-
-	ofGLFWWindowSettings settings;
-	settings.setGLVersion(3, 2);
-	settings.setSize(640, 720);
+	
+	int w = 640;
+	int h = 720;
+	
+	// setup the GL context
+#ifdef TARGET_OPENGLES
+	ofGLESWindowSettings settings;
+	settings.glesVersion = 2;
+	settings.setSize(w, h);
 	ofCreateWindow(settings);
+#else
+    ofGLFWWindowSettings settings;
+	settings.setGLVersion(3, 2);
+    settings.numSamples = 0;
+	settings.setSize(w, h);
+    ofCreateWindow(settings);                       
+#endif
 
-	ofRunApp(new ofApp());
+    // this kicks off the running of my app
+    // can be OF_WINDOW or OF_FULLSCREEN
+    // pass in width and height too:
+    ofRunApp(new ofApp());
 
 }
